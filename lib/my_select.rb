e03428f6869925f3207530186e4a3c9e4865f811
Akram Helil <akramhelil@gmail.com>
2019-01-15 01:00:29 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,12 +1,11 @@
 def my_select(collection)
- i = 0
- select = []
- while i < array.length
-    if yield(array[i])
-      select << array[i]
+  i = 0
+  new_collection = []
+  while i < collection.length
+    if yield(collection[i])
+      new_collection << collection[i]
     end
-    i+=1
+    i = i + 1
   end
-  select
- # your code here!
-end
+  new_collection
+ end
